8cd760a58b8e8c560d5e2d1ba862d0ccc2a9d8d1
Saidsuhail vp <suhailsaid689@gmail.com>
2025-03-06 16:16:41 +0530

    Time: 2 ms (65.02%), Space: 55.2 MB (17.17%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,6 +3,5 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s.split(' ').filter(word => word !== '').reverse().join(" ")
+      return s.split(' ').filter(word => word !== '').reverse().join(' ');
 };
-console.log(reverseWords("the sky is blue"))
