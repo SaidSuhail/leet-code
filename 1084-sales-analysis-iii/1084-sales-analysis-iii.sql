f1087e1f5a15619c63cb85cd87fe05c857598d6d
Saidsuhail vp <suhailsaid689@gmail.com>
2026-02-01 23:00:53 +0530

    Time: 2894 ms (14.54%), Space: 0B (100%) - LeetHub

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,4 @@
+/* Write your T-SQL query statement below */
+SELECT s.product_id, p.product_name FROM sales s JOIN product p ON s.product_id = p.product_id
+GROUP BY s.product_id, p.product_name HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) 
+<= '2019-03-31'
