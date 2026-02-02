e9c981baf1e037a24f67e03693100ce5eacf6b10
Saidsuhail vp <suhailsaid689@gmail.com>
2026-02-02 20:26:32 +0530

    Time: 488 ms (6.97%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
-select id,movie,description,rating from cinema  where id%2 != 0 and description != "boring" order by rating desc;
+/* Write your T-SQL query statement below */
+select id ,movie,description,rating from Cinema where description != 'boring' and id%2 =1 order by rating desc;
