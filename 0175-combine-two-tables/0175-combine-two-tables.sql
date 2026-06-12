bd5878941961e51955a10ec6524ee2a2402963c7
Saidsuhail vp <suhailsaid689@gmail.com>
2026-06-13 00:13:06 +0530

    Time: 455 ms (41.05%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,3 +1,2 @@
-# Write your MySQL query statement below
-select  firstName,lastName,city,state  from person left join Address on
- person.personId = Address.personId;
+/* Write your T-SQL query statement below */
+select A.firstName,A.lastName,B.city,B.state from Person A Left join Address B on A.personId = B.personId
